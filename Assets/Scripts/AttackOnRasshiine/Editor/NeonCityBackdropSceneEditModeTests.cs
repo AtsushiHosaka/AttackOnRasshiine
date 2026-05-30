cfb47cb6c7b3f69bac08307886bf971d3eafb541
Atsushi Hosaka <atsushi_hosaka@broca.co.jp>
2026-05-30 22:04:52 +0900

    [UI] Remove burger props from Neon City backdrop (#88)
    * Remove burger props from neon city backdrop
    
    * Make burger prop scene check case-insensitive

---
+++ Assets/Scripts/AttackOnRasshiine/Editor/NeonCityBackdropSceneEditModeTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AttackOnRasshiine.Editor
+{
+    public sealed class NeonCityBackdropSceneEditModeTests
+    {
+        private const string PrototypeScenePath = "Assets/Scenes/RasshiineRaidPrototype.unity";
+
+        [Test]
+        public void PrototypeSceneDoesNotPlaceBurgerProps()
+        {
+            var scene = EditorSceneManager.OpenScene(PrototypeScenePath, OpenSceneMode.Single);
+            var burgerNames = scene.GetRootGameObjects()
+                .SelectMany(EnumerateSceneObjects)
+                .Select(gameObject => gameObject.name)
+                .Where(name => name.IndexOf("burger", StringComparison.OrdinalIgnoreCase) >= 0)
+                .ToList();
+
+            CollectionAssert.IsEmpty(burgerNames);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+        }
+
+        private static IEnumerable<GameObject> EnumerateSceneObjects(GameObject root)
+        {
+            yield return root;
+            foreach (Transform child in root.transform)
+            {
+                foreach (var gameObject in EnumerateSceneObjects(child.gameObject))
+                {
+                    yield return gameObject;
+                }
+            }
+        }
+    }
+}
